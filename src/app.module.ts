d1e122b3b59f852e53c619295bf12ef9557b52e1
shuverida <bashtarov@outlook.com>
2023-09-10 17:41:06 +0300

    fixed bugs with .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'dotenv';
 
 import { AuthModule } from './auth/auth.module';
 import { UserEntity } from './auth/entity/user.entity';
@@ -10,18 +11,23 @@ import * as process from 'process';
 import { AppointmentsEntity } from './appointments/entity/appointments.entity';
 import { CardEntity } from './appointments/entity/card.entity';
 
+config();
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
-      username: 'postgres',
-      password: '5940530bbbb',
-      database: 'barber',
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [UserEntity, AppointmentsEntity, CardEntity],
       synchronize: true,
     }),
+    // ConfigModule.forRoot({
+    //   envFilePath: '.env',
+    // }),
     AuthModule,
     AppointmentsModule,
   ],
